52ac5e5a19457fac8230f7794d85311da1b667c5
thisisofficial <thisisofficiallyofficial@gmail.com>
2022-02-17 18:14:09 -0600

    Added Data Type Examples

---
+++ Data-Examples.js
@@ -0,0 +1,16 @@
+let f = true; //bool
+let fo = null; //null 
+let foo; //undefined
+let num = 3; //number
+const big = 2n ** 53n; //big int
+let str = 'hello'; //string
+let sym = Symbol('a'); //symbol
+
+//Non primitive data types
+
+const obj = {
+    foo: 'Fool',
+    fool: 'Foo',
+}
+
+
